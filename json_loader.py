e35e21b45e3d739639a4f5bf90855f4b37ac5eab
dgudtsov <denis@denis>
2020-07-20 15:57:10 +0300

    second update

--- json_loader.py
+++ json_loader.py
@@ -63,7 +63,7 @@ import pymysql.cursors
 # Connect to the database
 connection = pymysql.connect(host='10.31.74.222',
                              user='pcap',
-                             password='armentel',
+                             password='',
                              db='pcap',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
